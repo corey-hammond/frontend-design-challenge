aeef720b7c548469b1d5f07373396932e668e1a9
Corey Hammond <coreyjhammond@gmail.com>
2020-03-17 21:15:17 -0700

    Updates

---
+++ precache-manifest.97803e7525e241f2d2871e2f2b057ffc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b0fa7e383e3695711f3636826e4440",
+    "url": "/frontend-design-challenge/index.html"
+  },
+  {
+    "revision": "952a50da02ae304548fe",
+    "url": "/frontend-design-challenge/static/css/main.75bcedee.chunk.css"
+  },
+  {
+    "revision": "b5f80e1adf9e2fb80ef2",
+    "url": "/frontend-design-challenge/static/js/2.0aad945d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/frontend-design-challenge/static/js/2.0aad945d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "952a50da02ae304548fe",
+    "url": "/frontend-design-challenge/static/js/main.8b5fdf80.chunk.js"
+  },
+  {
+    "revision": "d5fd63dd7b920f299972",
+    "url": "/frontend-design-challenge/static/js/runtime-main.4a7c4ce0.js"
+  },
+  {
+    "revision": "c89a3c513d093b6f3850bde0bd9f16c5",
+    "url": "/frontend-design-challenge/static/media/bg.c89a3c51.png"
+  }
+]);
