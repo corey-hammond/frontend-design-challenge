6450aefa1f4347af4060cc64d7c1e2b2aa81e5a8
Corey Hammond <coreyjhammond@gmail.com>
2020-03-17 20:52:54 -0700

    ready for deployment

---
+++ build/precache-manifest.ade10a3af11cf84524f3ed43c2ee569a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94cad9afb8f1c9e3c7257f700499da87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e591421fadfd373a759d",
+    "url": "/static/css/main.f335476e.chunk.css"
+  },
+  {
+    "revision": "b5f80e1adf9e2fb80ef2",
+    "url": "/static/js/2.0aad945d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.0aad945d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e591421fadfd373a759d",
+    "url": "/static/js/main.8b5fdf80.chunk.js"
+  },
+  {
+    "revision": "07caf0c0ad398538e609",
+    "url": "/static/js/runtime-main.1584ba38.js"
+  },
+  {
+    "revision": "c89a3c513d093b6f3850bde0bd9f16c5",
+    "url": "/static/media/bg.c89a3c51.png"
+  }
+]);
